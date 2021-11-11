0da0307dbaa8df5bc51c83ed1ee48cd2ecd8c73e
yo-dst <yoann.danset@gmail.com>
2021-11-11 14:27:25 +0100

    :p

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from "react";
+
 import './App.css';
+import img1 from "./images/IMAGEUNE.jpg";
+import img2 from "./images/IMAGEDEUX.jpg";
+import img3 from "./images/IMAGETROIS.jpg";
+
+const imgs = [
+	img1,
+	img2,
+	img3
+]
+
+const Canvas = (props) => {
+	const canvasRef = useRef(null);
+	let x = 0;
+	let y = 0;
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const ctx = canvas.getContext("2d");
+
+		canvas.addEventListener("mousemove", e => {
+			x = e.offsetX;
+			y = e.offsetY;
+			//console.log("(" + x + ", " + y + ")");
+		})
+	}, []);
+
+	return (
+		<canvas ref={canvasRef} {...props}/>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const width = 1200;
+	const height = 700;
+
+	useEffect(() => {
+
+	}, []);
+
+  	return (
+		<div className="app">
+			<Canvas width={width} height={height} />
+		</div>
+  	);
 }
 
 export default App;
