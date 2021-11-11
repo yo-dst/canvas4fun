b89b27f2f0082746cc03106ff6dfed4680623080
Yoann Danset <ydanset@c1r3p9.42nice.fr>
2021-11-11 20:04:14 +0100

    ;p

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { useEffect, useRef } from "react";
+import ScratchCard from 'react-scratch-coupon'
 
 import './App.css';
 
-/* 
+import img from "./images/IMAGEDEUX.jpg";
+
+/*
+import brushImg from "./images/brush.png";
 import img1 from ;
 import img2 from ;
 import img3 from ;
@@ -18,18 +22,21 @@ const imgs = [
 	"https://picsum.photos/id/500/1200/700"
 ]
 
-function App() {
+const App = () => {
 	let indexBg = 1;
-	const switchFreq = 5000;
-	const sizeBrush = 20;
+	const switchFreq = 10000;
+	const sizeBrush = 25;
 	const width = 1200;
 	const height = 700;
 	const canvasFgRef = useRef(null);
 	const canvasBgRef = useRef(null);
 	let isDrawing = false;
 	let isRunning = true;
+	//const brush = new Image();
+	//brush.src = brushImg;
 
 	const scratch = (ctx, x, y) => {
+		console.log(x + ", " + y);
 		if (!isDrawing || !isRunning)
 			return (null);
 		ctx.fillStyle = "white";
@@ -37,6 +44,9 @@ function App() {
 		ctx.beginPath();
 		ctx.arc(x, y, sizeBrush, 0, 2 * Math.PI);
 		ctx.fill();
+		/* OR
+		ctx.drawImg(brush, )
+		*/
 	}
 
 	const drawImg = (ctx, srcImg) => {
@@ -87,6 +97,23 @@ function App() {
 			</div>
 		</div>
   	);
+}
+
+const App2 = () => {
+	return (
+		<ScratchCard width={300} height={300} cover={img}>
+		<form className="form" >
+			<h2>Hello There!</h2>
+			<h1><code>Coupon code : 1651613335</code></h1>
+			<div>
+				<input type="text" name="code" placeholder="Coupon Code"></input>
+			</div>
+			<div>
+				<input type="submit" value="Submit"></input>
+			</div>
+		</form>
+	  </ScratchCard>
+	);
 }
 
 export default App;
