f65fa2b42ad664db8b23281b9d088c805d778e1e
Yoann Danset <ydanset@c1r3p9.42nice.fr>
2021-11-11 18:55:47 +0100

    mmm ca m'a l'air carre tt ca

--- src/App.js
+++ src/App.js
@@ -1,48 +1,90 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 import './App.css';
-import img1 from "./images/IMAGEUNE.jpg";
-import img2 from "./images/IMAGEDEUX.jpg";
-import img3 from "./images/IMAGETROIS.jpg";
+
+/* 
+import img1 from ;
+import img2 from ;
+import img3 from ;
+import img4 from ;
+import img5 from ;
+*/
 
 const imgs = [
-	img1,
-	img2,
-	img3
+	"https://picsum.photos/id/10/1200/700",
+	"https://picsum.photos/id/40/1200/700",
+	"https://picsum.photos/id/100/1200/700",
+	"https://picsum.photos/id/200/1200/700",
+	"https://picsum.photos/id/500/1200/700"
 ]
 
-const Canvas = (props) => {
-	const canvasRef = useRef(null);
-	let x = 0;
-	let y = 0;
-
-	useEffect(() => {
-		const canvas = canvasRef.current;
-		const ctx = canvas.getContext("2d");
-
-		canvas.addEventListener("mousemove", e => {
-			x = e.offsetX;
-			y = e.offsetY;
-			//console.log("(" + x + ", " + y + ")");
-		})
-	}, []);
-
-	return (
-		<canvas ref={canvasRef} {...props}/>
-	);
-}
-
 function App() {
+	let indexBg = 1;
+	const switchFreq = 5000;
+	const sizeBrush = 20;
 	const width = 1200;
 	const height = 700;
+	const canvasFgRef = useRef(null);
+	const canvasBgRef = useRef(null);
+	let isDrawing = false;
+	let isRunning = true;
+
+	const scratch = (ctx, x, y) => {
+		if (!isDrawing || !isRunning)
+			return (null);
+		ctx.fillStyle = "white";
+		ctx.globalCompositeOperation = "destination-out";
+		ctx.beginPath();
+		ctx.arc(x, y, sizeBrush, 0, 2 * Math.PI);
+		ctx.fill();
+	}
+
+	const drawImg = (ctx, srcImg) => {
+		const img = new Image();
+		img.src = srcImg;
+		img.onload = () => {
+			ctx.drawImage(img, 0, 0, width, height);
+		};
+	}
 
 	useEffect(() => {
+		const canvasFg = canvasFgRef.current;
+		const ctxFg = canvasFg.getContext("2d");
 
+		const canvasBg = canvasBgRef.current;
+		const ctxBg = canvasBg.getContext("2d");
+
+		canvasFg.addEventListener("mousedown", () => { isDrawing = true });
+		canvasFg.addEventListener("mouseup", () => { isDrawing = false });
+		canvasFg.addEventListener("mousemove", e => {
+			scratch(ctxFg, e.offsetX, e.offsetY);
+		});
+
+		drawImg(ctxFg, imgs[0]);
+		drawImg(ctxBg, imgs[1]);
+
+		const interval = setInterval(() => {
+			indexBg++;
+			if (indexBg >= imgs.length)
+			{
+				console.log("here: ", indexBg);
+				isRunning = false;
+				clearInterval(interval);
+				return (null);
+			}
+
+			ctxFg.globalCompositeOperation = "destination-over";
+			ctxFg.drawImage(canvasBg, 0, 0);
+			drawImg(ctxBg, imgs[indexBg]);
+		}, switchFreq);
 	}, []);
 
   	return (
 		<div className="app">
-			<Canvas width={width} height={height} />
+			<div className="canvas-wrapper" style={{width: width, height: height}}>
+				<canvas ref={canvasFgRef} width={width} height={height} id="canvas-fg" />
+				<canvas ref={canvasBgRef} width={width} height={height} id="canvas-bg" />
+			</div>
 		</div>
   	);
 }
