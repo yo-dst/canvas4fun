c96f5fef192fc111d9bbe7e630d56d110ddee537
yo-dst <yoann.danset@gmail.com>
2021-11-16 15:11:27 +0100

    fock this

--- src/App.js
+++ src/App.js
@@ -1,14 +1,74 @@
+import { useEffect, useRef, useState } from 'react';
+
 import './App.css';
 import ScratchPad from "./ScratchPad";
 
+const imgs = [
+	"https://picsum.photos/id/10/1200/700",
+	"https://picsum.photos/id/40/1200/700",
+	"https://picsum.photos/id/100/1200/700",
+	"https://picsum.photos/id/200/1200/700",
+	"https://picsum.photos/id/500/1200/700"
+]
+
+const Child = (props) => {
+	const canvasRef = useRef(null);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const ctx = canvas.getContext("2d");
+
+		const img = new Image();
+		img.src = imgs[props.index];
+		img.onload = () => {
+			ctx.drawImage(img, 0, 0, props.width, props.height);
+		}
+	}, [props.index]);
+
+	return (
+		<div style={{color: "white"}}>
+			<canvas ref={canvasRef} width={props.width} height={props.height} />
+		</div>
+	)
+}
+
 const App = () => {
+	const [counter, setCounter] = useState(0);
+
+  	return (
+		<div className="app">
+			<button onClick={e => setCounter(counter + 1)}>Increment</button>
+			<Child width={500} height={500} index={counter} />
+		</div>
+  	);
+}
+
+const App2 = () => {
+	const [index, setIndex] = useState(0);
+	const switchFreq = 5000; // in ms
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setIndex(index => {
+				console.log(index);
+				if (index === imgs.length - 1)
+				{
+					clearInterval(interval);
+					return (index);
+				}
+				return (index + 1);
+			});
+		}, switchFreq);
+	}, []);
+
   	return (
 		<div className="app">
 			<ScratchPad
 				width={1200}
 				height={700}
 				sizeBrush={35}
-				switchFreq={7}
+				fg={imgs[index]}
+				bg={imgs[index + 1]}
 			/>
 		</div>
   	);
--- src/ScratchPad.js
+++ src/ScratchPad.js
@@ -1,33 +1,17 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
+import { useState } from "react/cjs/react.development";
 
 import "./App.css";
 import brush from "./images/brush0.png";
-
-const imgs = [
-	"https://picsum.photos/id/10/1200/700",
-	"https://picsum.photos/id/40/1200/700",
-	"https://picsum.photos/id/100/1200/700",
-	"https://picsum.photos/id/200/1200/700",
-	"https://picsum.photos/id/500/1200/700"
-]
-
-function distanceBetween(point1, point2) {
-	return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2));
-}
-
-function angleBetween(point1, point2) {
-	return Math.atan2(point2.x - point1.x, point2.y - point1.y);
-}
 
 const ScratchPad = (props) => {
 	const {
 		width,
 		height,
 		sizeBrush,
-		switchFreq
+		fg,
+		bg
 	} = props;
-
-	const [timer, setTimer] = useState(switchFreq);
 
 	const brushImg = new Image();
 	brushImg.src = brush;
@@ -35,38 +19,42 @@ const ScratchPad = (props) => {
 	const canvasFgRef = useRef(null);
 	const canvasBgRef = useRef(null);
 
+	const distanceBetween = (point1, point2) => {
+		return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2));
+	}
+	
+	const angleBetween = (point1, point2) => {
+		return Math.atan2(point2.x - point1.x, point2.y - point1.y);
+	}
+
+	const drawImg = (ctx, srcImg) => {
+		const img = new Image();
+		img.crossOrigin = "anonymous"; // no need that if images are local
+		img.src = srcImg;
+		img.onload = () => {
+			ctx.drawImage(img, 0, 0, width, height);
+		};
+	}
+
+	const scratch = (ctx, x, y) => {
+		ctx.globalCompositeOperation = "destination-out";
+		ctx.drawImage(brushImg, x, y, sizeBrush, sizeBrush);
+	}
+
+	const saveImg = (canvas) => {
+		const image = canvas.toDataURL();
+		var tmpLink = document.createElement('a');  
+		tmpLink.download = 'image.png';
+		tmpLink.href = image;  
+	
+		document.body.appendChild(tmpLink);
+		tmpLink.click();  
+		document.body.removeChild(tmpLink);
+	}
+
 	useEffect(() => {
-		let indexFg = 0;
-		let isRunning = true;
+		console.log("rerender pad");
 		let lastPoint;
-
-		const drawImg = (ctx, srcImg) => {
-			const img = new Image();
-			img.crossOrigin = "anonymous"; // no need that if images are local
-			img.src = srcImg;
-			img.onload = () => {
-				ctx.drawImage(img, 0, 0, width, height);
-			};
-		}
-
-		const scratch = (ctx, x, y) => {
-			ctx.globalCompositeOperation = "destination-out";
-			ctx.drawImage(brushImg, x, y, sizeBrush, sizeBrush);
-		}
-
-		const saveImg = () => {
-			ctxFg.globalCompositeOperation = "destination-over";
-			ctxFg.drawImage(canvasBg, 0, 0);
-
-			const image = canvasFg.toDataURL();
-			var tmpLink = document.createElement('a');  
-			tmpLink.download = 'image.png';
-			tmpLink.href = image;  
-		
-			document.body.appendChild(tmpLink);
-			tmpLink.click();  
-			document.body.removeChild(tmpLink);
-		}
 
 		const canvasFg = canvasFgRef.current;
 		const ctxFg = canvasFg.getContext("2d");
@@ -74,13 +62,10 @@ const ScratchPad = (props) => {
 		const canvasBg = canvasBgRef.current;
 		const ctxBg = canvasBg.getContext("2d");
 
-		drawImg(ctxFg, imgs[0]);
-		drawImg(ctxBg, imgs[1]);
+		drawImg(ctxFg, fg);
+		drawImg(ctxBg, bg);
 
 		canvasFg.addEventListener("mousemove", e => {
-			if (!isRunning)
-				return (null);
-
 			if (!lastPoint)
 				lastPoint = { x: e.offsetX, y: e.offsetY };
 
@@ -95,57 +80,13 @@ const ScratchPad = (props) => {
 			}
 			lastPoint = currentPoint;
 		});
-
-		const intervalId = setInterval(() => {
-			console.log(indexFg);
-			setTimer(timer => {
-				//console.log("here");
-				if (timer === 1)
-				{
-					//saveImg();
-					indexFg += 1;
-					if (indexFg >= imgs.length - 1) {
-						//console.log("here");
-						isRunning = false;
-						document.getElementsByClassName("canvas-wrapper")[0].classList.remove("hide-cursor");
-						return (null);
-					}
-					//console.log(indexFg);
-					drawImg(ctxBg, imgs[indexFg + 1]);
-					drawImg(ctxFg, imgs[indexFg]);
-					return (switchFreq);
-				}
-				else
-					return (timer - 1);
-			});
-			/*
-			indexBg++;
-			if (indexBg >= imgs.length)
-			{
-				ctxFg.globalCompositeOperation = "destination-over";
-				ctxFg.drawImage(canvasBg, 0, 0);
-				isRunning = false;
-				clearInterval(switchInterval);
-				
-				setShowAnnounce(true);
-				saveImg();
-				setTimeout(() => setShowAnnounce(false), 3000);
-				return ;
-			}
-
-			ctxFg.globalCompositeOperation = "destination-over";
-			ctxFg.drawImage(canvasBg, 0, 0);
-			drawImg(ctxBg, imgs[indexBg]);
-			*/
-		}, 1000);
-		return () => clearInterval(intervalId);
-	}, [switchFreq, sizeBrush, width, height]);
+	}, [sizeBrush, width, height, fg, bg]);
 
 	return (
 		<div className="canvas-wrapper hide-cursor" style={{width: width, height: height}}>
 			<canvas ref={canvasFgRef} width={width} height={height} id="canvas-fg" />
 			<canvas ref={canvasBgRef} width={width} height={height} id="canvas-bg" />
-			<div className="timer">{timer}</div>
+			
 		</div>
 	);
 }
